cfdfe67a77ce882d2dd910a20cc30b495b2658ec
Boris Fedorov <boris.fedorov.ru@gmail.com>
2025-04-23 11:23:28 +0300

    remove s

--- view/ui/form_templ.go
+++ view/ui/form_templ.go
@@ -65,7 +65,7 @@ func Form(p *FormProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</form>s")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</form>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
