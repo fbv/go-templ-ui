fc3331e3d00c1f79c25033dccad12a5bc5cd8345
Boris Fedorov <boris.fedorov@primeconcept.co.uk>
2025-04-24 10:50:43 +0300

    remove s

--- view/ui/form_templ.go
+++ view/ui/form_templ.go
@@ -124,7 +124,7 @@ func GridForm(p *FormProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "</div></form>s")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "</div></form>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
