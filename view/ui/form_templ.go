7a9cc7b117a18a39ce90928ceac9f5030fbbec06
Boris Fedorov <boris.fedorov@primeconcept.co.uk>
2025-04-24 12:02:21 +0300

    form align

--- view/ui/form_templ.go
+++ view/ui/form_templ.go
@@ -57,7 +57,7 @@ func Form(p *FormProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" class=\"max-w-sm mx-auto\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" class=\"max-w-sm\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
