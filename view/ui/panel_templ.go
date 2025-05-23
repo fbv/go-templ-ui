720ed886e9450403658a9414561174309a52f8fb
Boris Fedorov <boris.fedorov.ru@gmail.com>
2025-05-23 11:05:52 +0300

    add style

--- view/ui/panel_templ.go
+++ view/ui/panel_templ.go
@@ -28,7 +28,7 @@ func HorizontalPanel() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"flex\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
