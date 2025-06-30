cd03358a61742821cf172b8cf8782f9d962f3a9e
Boris Fedorov <boris.fedorov.ru@gmail.com>
2025-06-30 22:44:46 +0300

    adjust styles

--- view/ui/form_templ.go
+++ view/ui/form_templ.go
@@ -61,7 +61,7 @@ func Form(p *FormProps) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" class=\"max-w-sm\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" class=\"max-w-sm grid grid-cols-1 gap-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
