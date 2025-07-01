5cd3d279df213036834588f5a2dd80e90269561b
Boris Fedorov <boris.fedorov.ru@gmail.com>
2025-07-01 11:46:42 +0300

    adjust styles

--- view/ui/select_templ.go
+++ view/ui/select_templ.go
@@ -62,7 +62,7 @@ func Select(p *SelectProps) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"mb-5\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
